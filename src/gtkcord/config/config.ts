41f12d5c888a6cfcf5a2800fabaa29db91741a1e
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-04-02 00:32:34 -0500

    Move config.ts and finish config.ts for now

--- src/gtkcord/config/config.ts
+++ src/gtkcord/config/config.ts
@@ -1,3 +1,5 @@
+import { homedir } from 'os';
+import { mkdirSync, existsSync, readdirSync } from 'fs';
 
 export const WindowWidth	=	1000
 export const WindowHeight	=	700
@@ -17,3 +19,20 @@ export const ChannelWidth	=	240
 
 export const InputIconSize	=	234	// TODO: change this to the node-gtk equivalent of gtk.ICON_SIZE_LARGE_TOOLBAR
 
+export const DefaultEmbedColor = 0x808080
+
+var path: string;
+
+function init() {
+    path = homedir() + '/gktcord';
+
+    if(!existsSync(path)){
+        mkdirSync(path, {mode: '0o775'});
+    }
+}
+
+export function mustRead(dir:string) {
+    dir = path + dir;
+
+    return { files: readdirSync(dir), path: dir, undefined };
+}
